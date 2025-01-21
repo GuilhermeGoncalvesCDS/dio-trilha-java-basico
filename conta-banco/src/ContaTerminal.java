85f84e400ed952f8676ede5345c258385035b194
GuilhermeGoncalvesCDS <gui.goncalvescds@gmail.com>
2025-01-20 22:43:30 -0300

    finalContaTerminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -15,22 +15,22 @@ public class ContaTerminal {
             //Exibir a mensagem da conta criada
 
 
-    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+        System.out.println("Digite a sua Cota do banco: ");
+        int numeroConta = scanner.nextInt();
+        
+        System.out.println("Digite a sua agência: ");
+        String agenciaConta = scanner.next();
 
-    System.out.println("Digite a sua Cota do banco: ");
-    int numeroConta = scanner.nextInt();
-    
-    System.out.println("Digite a sua agência: ");
-    String agenciaConta = scanner.next();
+        System.out.println("Digite o seu nome completo: ");
+        String nomeCliente = scanner.next();
 
-    System.out.println("Digite o seu nome completo: ");
-    String nomeCliente = scanner.next();
-
-    System.out.println("Informe o saldo da sua conta: ");
-    float saldoConta = scanner.nextFloat();
+        System.out.println("Informe o saldo da sua conta: ");
+        float saldoConta = scanner.nextFloat();
 
 
-    System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agenciaConta + ", conta " + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque");
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agenciaConta + ", conta " + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque");
+    }
 
     }
 
